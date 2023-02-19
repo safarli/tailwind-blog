edbd09a2a40619349e59194e663f174e6dba099d
Safarli <wilecto@gmail.com>
2023-02-20 01:36:04 +0400

    a commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}"],
+  content: ['./public/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        kanit: ['Kanit', 'sans-serif'],
+        barlow: ['Barlow', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+        teko: ['Teko', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
-}
+};

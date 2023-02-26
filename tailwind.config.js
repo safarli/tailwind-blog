6c305069bb0d306775d2599b52cced4d621add55
Safarli <wilecto@gmail.com>
2023-02-26 14:25:15 +0400

    a commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
         barlow: ['Barlow', 'sans-serif'],
         inter: ['Inter', 'sans-serif'],
         teko: ['Teko', 'sans-serif'],
+        lobster: ['Lobster', 'cursive'],
       },
     },
   },
